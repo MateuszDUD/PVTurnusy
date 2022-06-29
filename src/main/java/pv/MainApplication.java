9063f5e551325b14014f01a0e6e3bf59c88fe14b
MartinaCer <martinacernekova@azet.sk>
2022-06-29 06:40:58 +0200

    viax garazi #1

--- src/main/java/pv/MainApplication.java
+++ src/main/java/pv/MainApplication.java
@@ -1,5 +1,6 @@
 package pv;
 
+import java.util.Arrays;
 import pv.alg.dijkstra.Dijkstra;
 import pv.alg.lp.LpSolver;
 import pv.util.DataReader;
@@ -8,6 +9,7 @@ import java.util.Map;
 import pv.alg.lp.LpModelBus;
 import pv.alg.lp.LpModelMinT;
 import pv.alg.lp.LpModelMinTGaraz;
+import pv.alg.lp.LpModelMinTGaraze;
 import pv.alg.lp.LpModelWriter;
 
 public class MainApplication {
@@ -22,7 +24,8 @@ public class MainApplication {
         
         //LpModelWriter writer = new LpModelBus(m, DataReader.readSpoje());
         //LpModelWriter writer = new LpModelMinT(m, DataReader.readSpoje());
-        LpModelWriter writer = new LpModelMinTGaraz(m, DataReader.readSpoje(), 470);
+        //LpModelWriter writer = new LpModelMinTGaraz(m, DataReader.readSpoje(), 470);
+        LpModelWriter writer = new LpModelMinTGaraze(m, DataReader.readSpoje(), Arrays.asList(470, 471));
         writer.createModel();
 
         LpSolver lp = new LpSolver();
