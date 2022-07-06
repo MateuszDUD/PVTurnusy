7574645268976b6647a39f38f99eafe3b501ddb3
MartinaCer <55285683+MartinaCer@users.noreply.github.com>
2022-07-06 10:49:46 +0200

    viac garazi fix

--- src/main/java/pv/alg/lp/LpModelMinTGaraze.java
+++ src/main/java/pv/alg/lp/LpModelMinTGaraze.java
@@ -24,7 +24,7 @@ public class LpModelMinTGaraze extends LpModelWriter {
 
     @Override
     public void createModel() {
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+        try ( Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(lp_model), "utf-8"))) {
             createLinearFunction(writer);
             createSubject(writer);
@@ -97,7 +97,7 @@ public class LpModelMinTGaraze extends LpModelWriter {
                 s += distance == 0
                         ? 1
                         : distance;
-                s += "v" + spoje2.get(i).getId();
+                s += "v" + spoje2.get(i).getId() + "_" + id;
                 if (s.length() > max_c) {
                     writer.write(s);
                     writer.write("\n");
@@ -116,66 +116,130 @@ public class LpModelMinTGaraze extends LpModelWriter {
         writer.write("Subject To\n");
 
         int cNumber = 0;
-        for (Integer id : idgaraze) {
-            for (int i = 0; i < spoje2.size(); i++) {
-                if (spoje2.get(i).getPossibleConnectionsFromThis().isEmpty()) {
-                    continue;
-                }
-                String s = "  ";
+
+        for (int i = 0; i < spoje2.size(); i++) {
+            if (spoje2.get(i).getPossibleConnectionsFromThis().isEmpty()) {
+                continue;
+            }
+            for (int j = 0; j < spoje2.get(i).getPossibleConnectionsFromThis().size(); j++) {
                 writer.write(" c" + cNumber++ + ":\n");
-                int j = 0;
-                s += "u" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId() + "_" + id;
-                while (j < spoje2.get(i).getPossibleConnectionsFromThis().size()) {
+                writer.write("u" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId() + " = ");
+                String s = " ";
+                for (Integer id : idgaraze) {
                     if (!s.equals(" ")) {
                         s += "+";
                     }
-
-                    s += "x" + spoje2.get(i).getId() + "_" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId() + "_" + id;
-
+                    s += "u" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId() + "_" + id;
                     if (s.length() > max_c) {
                         writer.write(s);
                         writer.write("\n");
-                        s = "  ";
+                        s = " ";
                     }
-                    j++;
                 }
-
-                if (!s.equals(" ")) {
-                    writer.write(s);
-                    writer.write("\n");
-                }
-                writer.write("  = 1\n");
+                writer.write("\n");
             }
         }
-        for (Integer id : idgaraze) {
-            for (int i = 0; i < spoje2.size(); i++) {
-                if (spoje2.get(i).getPossibleConnectionsToThis().isEmpty()) {
-                    continue;
+
+        for (int i = 0; i < spoje2.size(); i++) {
+            if (spoje2.get(i).getPossibleConnectionsToThis().isEmpty()) {
+                continue;
+            }
+            writer.write(" c" + cNumber++ + ":\n");
+            writer.write("v" + spoje2.get(i).getId() + " = ");
+            String s = " ";
+            for (Integer id : idgaraze) {
+                if (!s.equals(" ")) {
+                    s += "+";
                 }
-
-                writer.write(" c" + cNumber++ + ":\n");
-                String s = "  ";
                 s += "v" + spoje2.get(i).getId() + "_" + id;
+                if (s.length() > max_c) {
+                    writer.write(s);
+                    writer.write("\n");
+                    s = " ";
+                }
+            }
+            writer.write("\n");
+        }
 
-                for (int j = 0; j < spoje2.get(i).getPossibleConnectionsToThis().size(); j++) {
+        for (int i = 0; i < spoje2.size(); i++) {
+            for (int j = 0; j < spoje2.get(i).getPossibleConnectionsFromThis().size(); j++) {
+                writer.write(" c" + cNumber++ + ":\n");
+                writer.write("x" + spoje2.get(i).getId() + "_" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId() + " = ");
+                String s = " ";
+                for (Integer id : idgaraze) {
                     if (!s.equals(" ")) {
                         s += "+";
                     }
-
-                    s += "x" + spoje2.get(i).getPossibleConnectionsToThis().get(j).getId() + "_" + spoje2.get(i).getId() + "_" + id;
-
+                    s += "x" + spoje2.get(i).getId() + "_" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId() + "_" + id;
                     if (s.length() > max_c) {
                         writer.write(s);
                         writer.write("\n");
-                        s = "  ";
+                        s = " ";
                     }
                 }
+                writer.write("\n");
+            }
+        }
+
+        for (int i = 0; i < spoje2.size(); i++) {
+            if (spoje2.get(i).getPossibleConnectionsFromThis().isEmpty()) {
+                continue;
+            }
+
+            writer.write(" c" + cNumber++ + ":\n");
+            String s = "  ";
+
+            int j = 0;
+            s += "u" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId();
+            while (j < spoje2.get(i).getPossibleConnectionsFromThis().size()) {
                 if (!s.equals(" ")) {
+                    s += "+";
+                }
+
+                s += "x" + spoje2.get(i).getId() + "_" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId();
+
+                if (s.length() > max_c) {
                     writer.write(s);
                     writer.write("\n");
+                    s = "  ";
                 }
-                writer.write("  = 1\n");
+                j++;
             }
+
+            if (!s.equals(" ")) {
+                writer.write(s);
+                writer.write("\n");
+            }
+            writer.write("  = 1\n");
+        }
+
+        for (int i = 0; i < spoje2.size(); i++) {
+            if (spoje2.get(i).getPossibleConnectionsToThis().isEmpty()) {
+                continue;
+            }
+
+            writer.write(" c" + cNumber++ + ":\n");
+            String s = "  ";
+            s += "v" + spoje2.get(i).getId();
+
+            for (int j = 0; j < spoje2.get(i).getPossibleConnectionsToThis().size(); j++) {
+                if (!s.equals(" ")) {
+                    s += "+";
+                }
+
+                s += "x" + spoje2.get(i).getPossibleConnectionsToThis().get(j).getId() + "_" + spoje2.get(i).getId();
+
+                if (s.length() > max_c) {
+                    writer.write(s);
+                    writer.write("\n");
+                    s = "  ";
+                }
+            }
+            if (!s.equals(" ")) {
+                writer.write(s);
+                writer.write("\n");
+            }
+            writer.write("  = 1\n");
         }
 
         for (Integer id : idgaraze) {
@@ -230,32 +294,30 @@ public class LpModelMinTGaraze extends LpModelWriter {
             }
         }
 
-        for (Integer id : idgaraze) {
-            writer.write(" c" + cNumber++ + ":\n");
-            String s = "  ";
-            for (int i = 0; i < spoje2.size(); i++) {
-                for (int j = 0; j < spoje2.get(i).getPossibleConnectionsFromThis().size(); j++) {
+        writer.write(" c" + cNumber++ + ":\n");
+        String s = "  ";
+        for (int i = 0; i < spoje2.size(); i++) {
+            for (int j = 0; j < spoje2.get(i).getPossibleConnectionsFromThis().size(); j++) {
 
-                    if (!s.equals("  ")) {
-                        s += "+";
-                    }
+                if (!s.equals("  ")) {
+                    s += "+";
+                }
 
-                    s += "x" + spoje2.get(i).getId() + "_" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId() + "_" + id;
+                s += "x" + spoje2.get(i).getId() + "_" + spoje2.get(i).getPossibleConnectionsFromThis().get(j).getId();
 
-                    if (s.length() > max_c) {
-                        writer.write(s);
-                        writer.write("\n");
-                        s = "  ";
-                    }
+                if (s.length() > max_c) {
+                    writer.write(s);
+                    writer.write("\n");
+                    s = "  ";
                 }
             }
-            if (!s.equals("  ")) {
-                writer.write(s);
-                writer.write("\n");
-            }
-            writer.write("  = 777");
-
+        }
+        if (!s.equals("  ")) {
+            writer.write(s);
             writer.write("\n");
         }
+        writer.write("  = 777");
+
+        writer.write("\n");
     }
 }
